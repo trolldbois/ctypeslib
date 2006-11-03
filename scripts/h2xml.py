77abbe3816cb8b8d860a5e69462789b9a441d455
thomas.heller <thomas.heller@6015fed2-1504-0410-9fe1-9d1591cc4771>
2006-11-03 08:37:11 +0000

    Set executable property, ctypes_codegen was renamed.
    git-svn-id: http://svn.python.org/projects/ctypes/trunk/ctypeslib@52605 6015fed2-1504-0410-9fe1-9d1591cc4771

--- scripts/h2xml.py
+++ scripts/h2xml.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-from ctypes_codegen.h2xml import main
+from ctypeslib.codegen.h2xml import main
 
 if __name__ == "__main__":
     sys.exit(main())
--- scripts/xml2py.py
+++ scripts/xml2py.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
-from ctypes_codegen.xml2py import main
+from ctypeslib.codegen.xml2py import main
 
 if __name__ == "__main__":
     sys.exit(main())
