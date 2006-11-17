4e05e194c66d9274990ba23b1a52f76f8f8c4eca
thomas.heller <thomas.heller@6015fed2-1504-0410-9fe1-9d1591cc4771>
2006-11-17 14:53:42 +0000

    Repair and cleanup byref_offset.
    git-svn-id: http://svn.python.org/projects/ctypes/trunk/ctypeslib@52777 6015fed2-1504-0410-9fe1-9d1591cc4771

--- ctypeslib/test/byref_offset.py
+++ ctypeslib/test/byref_offset.py
@@ -1,29 +1,29 @@
 from ctypes import *
 
-# The definition of PyCArgObject (that is the type of object that
-# byref returns):
-class PyCArgObject(Structure):
-    class value(Union):
-        _fields_ = [("c", c_char),
-                    ("h", c_short),
-                    ("i", c_int),
-                    ("l", c_long),
-                    ("q", c_longlong),
-                    ("d", c_double),
-                    ("f", c_float),
-                    ("p", c_void_p)]
-    # Thanks to Lenard Lindstrom for this tip: The sizeof(PyObject_HEAD)
-    # is the same as object.__basicsize__.
-    _fields_ = [("PyObject_HEAD", c_byte * object.__basicsize__),
-                ("pffi_type", c_void_p),
-                ("tag", c_char),
-                ("value", value),
-                ("obj", c_void_p),
-                ("size", c_int)]
+def _calc_offset():
+    # The definition of PyCArgObject (that is the type of object that
+    # byref returns):
+    class PyCArgObject(Structure):
+        class value(Union):
+            _fields_ = [("c", c_char),
+                        ("h", c_short),
+                        ("i", c_int),
+                        ("l", c_long),
+                        ("q", c_longlong),
+                        ("d", c_double),
+                        ("f", c_float),
+                        ("p", c_void_p)]
+        # Thanks to Lenard Lindstrom for this tip: The sizeof(PyObject_HEAD)
+        # is the same as object.__basicsize__.
+        _fields_ = [("PyObject_HEAD", c_byte * object.__basicsize__),
+                    ("pffi_type", c_void_p),
+                    ("tag", c_char),
+                    ("value", value),
+                    ("obj", c_void_p),
+                    ("size", c_int)]
 
-    _anonymous_ = ["value"]
+        _anonymous_ = ["value"]
 
-def _validate():
     # additional checks to make sure that everything works as expected
 
     if sizeof(PyCArgObject) != type(byref(c_int())).__basicsize__:
@@ -41,7 +41,7 @@ def _validate():
 
     return PyCArgObject.p.offset # offset of the pointer field
 
-_byref_pointer_offset = _validate()
+_byref_pointer_offset = _calc_offset()
 
 def byref_offset(obj, offset):
     """byref_offset(cobj, offset) behaves similar this C code:
@@ -53,7 +53,7 @@ def byref_offset(obj, offset):
     """
     ref = byref(obj)
     # correct pointer field in the created byref object by 'offset'
-    c_void_p.from_address(id(ref) + 16).value += offset
+    c_void_p.from_address(id(ref) + _byref_pointer_offset).value += offset
     return ref
 
 __all__ = ["byref_offset"]
