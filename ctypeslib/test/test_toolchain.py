6f5e987b5a102b0c4b0bfcd88c17faab00655142
thomas.heller <thomas.heller@6015fed2-1504-0410-9fe1-9d1591cc4771>
2008-01-31 11:03:52 +0000

    Add a test for stdio.h.
    git-svn-id: http://svn.python.org/projects/ctypes/trunk/ctypeslib@60472 6015fed2-1504-0410-9fe1-9d1591cc4771

--- ctypeslib/test/test_toolchain.py
+++ ctypeslib/test/test_toolchain.py
@@ -4,7 +4,7 @@ from ctypeslib import h2xml, xml2py
 
 class ToolchainTest(unittest.TestCase):
     if sys.platform == "win32":
-        def test(self):
+        def test_windows(self):
             h2xml.main(["h2xml", "-q",
                         "-D WIN32_LEAN_AND_MEAN",
                         "-D _UNICODE", "-D UNICODE",
@@ -12,6 +12,16 @@ class ToolchainTest(unittest.TestCase):
                         "-o", "_windows_gen.xml"])
             xml2py.main(["xml2py", "_windows_gen.xml", "-w", "-o", "_winapi_gen.py"])
             import _winapi_gen
+
+    def test(self):
+        h2xml.main(["h2xml", "-q",
+                    "-D WIN32_LEAN_AND_MEAN",
+                    "-D _UNICODE", "-D UNICODE",
+                    "-c", "stdio.h",
+                    "-o", "_stdio_gen.xml"])
+        xml2py.main(["xml2py", "_stdio_gen.xml", "-o", "_stdio_gen.py"])
+        import _stdio_gen
+
 
 if __name__ == "__main__":
     import unittest
