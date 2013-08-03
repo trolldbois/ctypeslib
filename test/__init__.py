473881b5578c5eecb44f2039e039af8d90ad3979
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2013-08-03 02:48:42 -0400

    move unit tests

---
+++ test/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Unit test module."""
+
+__author__ = "Loic Jaquemet"
+__copyright__ = "Copyright (C) 2013 Loic Jaquemet"
+__email__ = "loic.jaquemet+python@gmail.com"
+__license__ = "GPL"
+__maintainer__ = "Loic Jaquemet"
+__status__ = "Production"
+
+import sys
+if sys.version_info < (2, 7):
+  import unittest2 as unittest
+else:
+  import unittest
+
+from run_src_app import run_app_test
+from run_src_app import makeTests
+
+def alltests():
+  makeTests()
+  ret = unittest.TestLoader().discover('test/')
+  return ret
+
+if __name__ == '__main__':
+  unittest.main(verbosity=0)
+
