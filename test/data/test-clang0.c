8a82e54a6358895872e4718503b14b92c34396e6
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2013-09-07 17:43:25 -0400

    crazy long double

---
+++ test/data/test-clang0.c
@@ -0,0 +1,11 @@
+/* very simple tests */
+
+typedef int badaboum;
+typedef unsigned int you_badaboum;
+typedef long big_badaboum;
+typedef unsigned long you_big_badaboum;
+typedef double double_badaboum;
+typedef long double long_double_badaboum;
+typedef float float_badaboum;
+typedef void* ptr;
+
--- test/test_record.py
+++ test/test_record.py
@@ -1,52 +1,29 @@
-import sys
-import os
 import unittest
-import tempfile
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
-
 import ctypes
-from ctypes.util import find_library
-from ctypeslib import clang2py
-from ctypeslib.codegen.codegenerator import generate_code
 
 from util import get_cursor
 from util import get_tu
-
-class ADict(dict):
-    def __getattr__(self, name):
-        try:
-            return self[name]
-        except KeyError:
-            raise AttributeError(name)
-
-class ArchTest(unittest.TestCase):
-    word_size = None
-    flags = []
-    def gen(self, fname, flags=None):
-            
-        ofi = StringIO()
-        #ofi = sys.stdout
-        generate_code( [fname], ofi, flags=flags or []) 
-        namespace = {}
-        print ofi.getvalue()
-        exec ofi.getvalue() in namespace
-        return ADict(namespace)
-
-
-class X32Test(ArchTest):
-    flags = ['-target','i386-linux']
-
-class X64Test(ArchTest):
-    flags = ['-target','x86_64-linux']
+from util import ArchTest
     
-    
-class RecordTestX32(X32Test):
-    def test_simple_records(self):
-        self.namespace = self.gen('test/data/test-ctypes0.c')
+'''Test if records are correctly generated for different target archictecture.
+'''
+class RecordTest(ArchTest):
+    def test_simple_x32(self):
+        flags = ['-target','i386-linux']
+        self.namespace = self.gen('test/data/test-clang0.c', flags)
+        self.assertEquals(ctypes.sizeof(self.namespace.badaboum), 4)
+        self.assertEquals(ctypes.sizeof(self.namespace.you_badaboum), 4)
+        self.assertEquals(ctypes.sizeof(self.namespace.big_badaboum), 4)
+        self.assertEquals(ctypes.sizeof(self.namespace.you_big_badaboum), 4)
+        self.assertEquals(ctypes.sizeof(self.namespace.double_badaboum), 4)
+        self.assertEquals(ctypes.sizeof(self.namespace.long_double_badaboum), 4)
+        self.assertEquals(ctypes.sizeof(self.namespace.float_badaboum), 4)
+        self.assertEquals(ctypes.sizeof(self.namespace.ptr), 4)
 
+    @unittest.skip('')
+    def test_records_x32(self):
+        flags = ['-target','i386-linux']
+        self.namespace = self.gen('test/data/test-clang1.c', flags)
         self.assertEquals(ctypes.sizeof(self.namespace.structName), 14)
         self.assertEquals(ctypes.sizeof(self.namespace.structName2), 16)
         self.assertEquals(ctypes.sizeof(self.namespace.Node), 16)
@@ -56,8 +33,10 @@ class RecordTestX32(X32Test):
         self.assertEquals(ctypes.sizeof(self.namespace.Node5), 8)
         self.assertEquals(ctypes.sizeof(self.namespace.my_bitfield), 16)
 
+    @unittest.skip('')
     def test_padding(self):
-        self.namespace = self.gen('test/data/test-clang5.c')
+        flags = ['-target','i386-linux']
+        self.namespace = self.gen('test/data/test-clang5.c', flags)
         
         self.assertEquals(ctypes.sizeof(self.namespace.structName), 14)
         self.assertEquals(ctypes.sizeof(self.namespace.structName2), 16)
