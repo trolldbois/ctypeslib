1a97bb6360bad8a0bf6ee76ddc2750f938edf192
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2015-05-29 17:49:17 -0600

    clean some style issues

--- ctypeslib/codegen/typehandler.py
+++ ctypeslib/codegen/typehandler.py
@@ -238,7 +238,7 @@ class TypeHandler(ClangHandler):
         if self.is_registered(name):
             obj = self.get_registered(name)
         else:
-            log.debug('Was in RECORD but had to parse record declaration for %s'%(name))
+            log.debug('Was in RECORD but had to parse record declaration for %s',name)
             obj = self.parse_cursor(_decl)
         return obj
 
