acbc05e3b01ebf700025d187f3b39f05f091beb4
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2017-06-13 17:21:09 -0600

    release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="ctypeslib2",
-    version="2.1.5rc0",
+    version="2.1.5",
     description="ctypeslib2 - FFI toolkit, relies on clang",
     long_description=open("README.md").read(),
     author="Loic Jaquemet",
@@ -30,6 +30,6 @@ setup(
     scripts=['scripts/clang2py'],
     test_suite="test.alltests",
     install_requires=[
-       'clang>=4.0',
+       'clang>=3.7',
     ]
 )
