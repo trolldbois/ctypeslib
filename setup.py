7b7d697dc3be650020a8217564e96194e87e158e
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2021-02-22 21:53:56 -0700

    bump pre release to clang 11, py3 only

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="ctypeslib2",
-    version="2.2.3",
+    version="2.3.0-pre1",
     description="ctypeslib2 - FFI toolkit, relies on clang",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -15,8 +15,7 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     url="https://github.com/trolldbois/ctypeslib",
@@ -37,7 +36,7 @@ setup(
         ]},
     test_suite="test.alltests",
     install_requires=[
-       'clang>=7.0',
+       'clang>=11',
        'six'
     ]
 )
