0fe372869d9eb9bd3e110a3415313295babcfc6e
Romain Viollette <rviollette@logitech.com>
2021-03-10 01:18:58 +0100

    Test signed and unsigned enum value & size

--- test/data/test-enum.c
+++ test/data/test-enum.c
@@ -4,18 +4,3 @@ ONE,
 TWO,
 FOUR = 4
 };
-
-enum myEnum_byte {
-MY_ENUM_BYTE_ZERO,
-MY_ENUM_BYTE_MAX=0xFF
-};
-
-enum myEnum_int16 {
-MY_ENUM_INT16_ZERO,
-MY_ENUM_INT16_MAX=0xFFFF
-};
-
-enum myEnum_int32 {
-MY_ENUM_INT32_ZERO,
-MY_ENUM_INT32_MAX=0xFFFFFFFF
-};
--- test/test_enum.py
+++ test/test_enum.py
@@ -19,7 +19,7 @@ class EnumTest(ClangTest):
         self.assertEqual(self.namespace.ONE, 1)
         self.assertEqual(self.namespace.FOUR, 4)
 
-    def test_enum_short_option(self):
+    def test_enum_short_option_uint8(self):
         """
         Enums can be forced to occupy less space than an int if possible:
           1) Add the attribute `__attribute__((__packed__))` to the C variable declarations
@@ -27,44 +27,123 @@ class EnumTest(ClangTest):
         In any case, we should trust the enum size returned by the compiler.
         """
         flags = ['-target', 'i386-linux', '-fshort-enums']
-        self.gen('test/data/test-enum.c', flags)
+        self.convert(
+            '''
+        enum myEnum {
+            MIN=0,   /* UINT8_MIN */
+            MAX=0xFF /* UINT8_MAX */
+        };
+        ''', flags)
 
         # Expect enum stored as 1 byte
-        self.assertEqual(ctypes.sizeof(self.namespace.myEnum_byte), 1)
-        self.assertEqual(self.namespace.MY_ENUM_BYTE_ZERO, 0)
-        self.assertEqual(self.namespace.MY_ENUM_BYTE_MAX, 0xFF)
+        self.assertEqual(ctypes.sizeof(self.namespace.myEnum), 1)
+        self.assertEqual(self.namespace.MIN, 0)
+        self.assertEqual(self.namespace.MAX, 0xFF)
 
-        # Expect enum stored in 2 bytes
-        self.assertEqual(ctypes.sizeof(self.namespace.myEnum_int16), 2)
-        self.assertEqual(self.namespace.MY_ENUM_INT16_ZERO, 0)
-        self.assertEqual(self.namespace.MY_ENUM_INT16_MAX, 0xFFFF)
-
-        # Expect enum stored in 4 bytes
-        self.assertEqual(ctypes.sizeof(self.namespace.myEnum_int32), 4)
-        self.assertEqual(self.namespace.MY_ENUM_INT32_ZERO, 0)
-        self.assertEqual(self.namespace.MY_ENUM_INT32_MAX, 0xFFFFFFFF)
-
-    def test_enum_no_short_option(self):
+    def test_enum_short_option_uint16(self):
         """
-        By default, enums are stored as 'int', so they will occupy 4 bytes.
+        Enums can be forced to occupy less space than an int if possible:
+          1) Add the attribute `__attribute__((__packed__))` to the C variable declarations
+          2) Set the compiler flag ` CFLAGS += -fshort-enums`
+        In any case, we should trust the enum size returned by the compiler.
         """
-        flags = ['-target', 'i386-linux']
-        self.gen('test/data/test-enum.c', flags)
+        flags = ['-target', 'i386-linux', '-fshort-enums']
+        self.convert(
+            '''
+        enum myEnum {
+            MIN=0,      /* UINT16_MIN */
+            MAX=0xFFFF  /* UINT16_MAX */
+        };
+        ''', flags)
 
-        # Expect enum stored as 'int'
-        self.assertEqual(ctypes.sizeof(self.namespace.myEnum_byte), 4)
-        self.assertEqual(self.namespace.MY_ENUM_BYTE_ZERO, 0)
-        self.assertEqual(self.namespace.MY_ENUM_BYTE_MAX, 0xFF)
+        # Expect enum stored as 1 byte
+        self.assertEqual(ctypes.sizeof(self.namespace.myEnum), 2)
+        self.assertEqual(self.namespace.MIN, 0)
+        self.assertEqual(self.namespace.MAX, 0xFFFF)
 
-        # Expect enum stored as 'int'
-        self.assertEqual(ctypes.sizeof(self.namespace.myEnum_int16), 4)
-        self.assertEqual(self.namespace.MY_ENUM_INT16_ZERO, 0)
-        self.assertEqual(self.namespace.MY_ENUM_INT16_MAX, 0xFFFF)
+    def test_enum_short_option_uint32(self):
+        """
+        Enums can be forced to occupy less space than an int if possible:
+          1) Add the attribute `__attribute__((__packed__))` to the C variable declarations
+          2) Set the compiler flag ` CFLAGS += -fshort-enums`
+        In any case, we should trust the enum size returned by the compiler.
+        """
+        flags = ['-target', 'i386-linux', '-fshort-enums']
+        self.convert(
+            '''
+        enum myEnum {
+            MIN=0,          /* UINT32_MIN */
+            MAX=0xFFFFFFFF  /* UINT32_MAX */
+        };
+        ''', flags)
 
-        # Expect enum stored as 'int'
-        self.assertEqual(ctypes.sizeof(self.namespace.myEnum_int32), 4)
-        self.assertEqual(self.namespace.MY_ENUM_INT32_ZERO, 0)
-        self.assertEqual(self.namespace.MY_ENUM_INT32_MAX, 0xFFFFFFFF)
+        # Expect enum stored as 1 byte
+        self.assertEqual(ctypes.sizeof(self.namespace.myEnum), 4)
+        self.assertEqual(self.namespace.MIN, 0)
+        self.assertEqual(self.namespace.MAX, 0xFFFFFFFF)
+
+    def test_enum_short_option_int8(self):
+        """
+        Enums can be forced to occupy less space than an int if possible:
+          1) Add the attribute `__attribute__((__packed__))` to the C variable declarations
+          2) Set the compiler flag ` CFLAGS += -fshort-enums`
+        In any case, we should trust the enum size returned by the compiler.
+        """
+        flags = ['-target', 'i386-linux', '-fshort-enums']
+        self.convert(
+            '''
+        enum myEnum {
+            MIN=-128, /* INT8_MIN */
+            MAX= 127  /* INT8_MAX */
+        };
+        ''', flags)
+
+        # Expect enum stored as 1 byte
+        self.assertEqual(ctypes.sizeof(self.namespace.myEnum), 1)
+        self.assertEqual(self.namespace.MIN, -128)
+        self.assertEqual(self.namespace.MAX, 127)
+
+    def test_enum_short_option_int16(self):
+        """
+        Enums can be forced to occupy less space than an int if possible:
+          1) Add the attribute `__attribute__((__packed__))` to the C variable declarations
+          2) Set the compiler flag ` CFLAGS += -fshort-enums`
+        In any case, we should trust the enum size returned by the compiler.
+        """
+        flags = ['-target', 'i386-linux', '-fshort-enums']
+        self.convert(
+            '''
+        enum myEnum {
+            MIN=-32768, /* INT16_MIN */
+            MAX= 32767  /* INT16_MAX*/
+        };
+        ''', flags)
+
+        # Expect enum stored as 1 byte
+        self.assertEqual(ctypes.sizeof(self.namespace.myEnum), 2)
+        self.assertEqual(self.namespace.MIN, -32768)
+        self.assertEqual(self.namespace.MAX, 32767)
+
+    def test_enum_short_option_int32(self):
+        """
+        Enums can be forced to occupy less space than an int if possible:
+          1) Add the attribute `__attribute__((__packed__))` to the C variable declarations
+          2) Set the compiler flag ` CFLAGS += -fshort-enums`
+        In any case, we should trust the enum size returned by the compiler.
+        """
+        flags = ['-target', 'i386-linux', '-fshort-enums']
+        self.convert(
+            '''
+        enum myEnum {
+            MIN=-65536, /* INT32_MIN */
+            MAX= 65535  /* INT32_MAX*/
+        };
+        ''', flags)
+
+        # Expect enum stored as 1 byte
+        self.assertEqual(ctypes.sizeof(self.namespace.myEnum), 4)
+        self.assertEqual(self.namespace.MIN, -65536)
+        self.assertEqual(self.namespace.MAX, 65535)
 
 
 if __name__ == "__main__":
