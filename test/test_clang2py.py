985c6db45dfcb8659740f80b3571ea992b53f8df
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2025-02-18 06:07:59 -0700

    Fix unittest for clang19, enum naming is better

--- test/test_clang2py.py
+++ test/test_clang2py.py
@@ -302,8 +302,10 @@ class OrderingTest(ClangTest):
     def test_enum_struct(self):
         """run clang2py on a ordering issue involving enum and struct"""
         p, output, stderr = clang2py(['./test/data/test-enum.c'])
-        decl = output.index("('e', c__EA_E),")
-        enum = output.index("c__EA_E = ctypes.c_uint32")
+        # decl = output.index("('e', c__EA_E),")  # Fixed in clang 19
+        # enum = output.index("c__EA_E = ctypes.c_uint32")
+        decl = output.index("('e', E),")  # Fixed in clang 19
+        enum = output.index("E = ctypes.c_uint32")
         self.assertGreater(decl, enum, "Generated incorrect ordering")
 
 
